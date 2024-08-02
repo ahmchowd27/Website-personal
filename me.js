37392e379b1bd62f9c5cb1144a4949fea78fb5dd
Ahmed Sofa Chowdhury <112588301+ahmchowd27@users.noreply.github.com>
2024-08-01 22:01:49 -0400

    Add files via upload

---
+++ me.js
@@ -0,0 +1,7 @@
+const menutoggle= document.querySelector('.menu-toggle');
+const nav= document.querySelector('nav');
+
+
+menutoggle.addEventListener('click',()=>{
+    nav.classList.toggle('open');
+});
